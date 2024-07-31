6039f2a12cf3be53c54ee69556c46e00a2d89f42
David Vazquez <capacitaciones06.13@adviters.com>
2024-07-31 10:52:46 -0300

    Home capturando input}

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from "react";
 
-
-export const Category = () => {
+export const Category = ({ category, checked, onChange }) => {
   return (
-    
-        <>
-         <label style={{display:"flex",alignItems:"center",justifyContent:"space-evenly",gap:"3px "}} >
-            <span style={{fontSize:"20px"}} className="label-text">action</span>
-            {/* <input type="checkbox" className='category-cb' /> */}
-            <input type="checkbox" defaultChecked className="checkbox checkbox-info checkbox-xs" />
-        </label>
+    <>
+      <label
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          gap: "3px ",
+        }}
+      >
+        <span style={{ fontSize: "20px" }} className="label-text">
+          {category}
+        </span>
+        {/* <input type="checkbox" className='category-cb' /> */}
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          className="checkbox checkbox-info checkbox-xs"
+        />
+      </label>
 
-{/* <div className="form-control">
+      {/* <div className="form-control">
 <label className="cursor-pointer label">
     <span className="label-text">Remember me</span>
     <input type="checkbox" defaultChecked className="checkbox checkbox-info" />
 </label>
 </div> */}
-   
-     </>
-  )
-}
+    </>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,84 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import { Carrousel } from '../../components/carrousel/Carrousel'
-import "./home.css"
-import { Card } from '../../components/card/Card'
-import{InputSearch} from "../../components/inputSearch/InputSearch"
-import { Category } from '../../components/category/Category'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Carrousel } from "../../components/carrousel/Carrousel";
+import "./home.css";
+import { Card } from "../../components/card/Card";
+import { InputSearch } from "../../components/inputSearch/InputSearch";
+import { Category } from "../../components/category/Category";
+import axios from "axios";
 
 export const Home = () => {
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState({});
 
+  useEffect(() => {
+    const url = "../../../data.json";
 
-  const [data,setData] = useState([])
-
-  useEffect(()=>{
- const url = "../../../data.json"
-
-      const fetchData = async()=>{
-        try{
-          const response = await axios.get("../../../data.json")
-          console.log(data)
-          setData(response.data)
-        }catch(error){
-          console.log(error)
-        }
-
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("../../../data.json");
+        setData(response.data);
+        const allCategories = response.data.map((item) => item.category);
+        console.log(allCategories);
+        setCategory(Array.from(new Set(allCategories)));
+      } catch (error) {
+        console.log(error);
       }
-      fetchData()
-  },[])
+    };
+    fetchData();
+  }, []);
 
-  // const getmovies = async()=>{
-  //   const url = "../../../data.json"
-  //   try{
-  //     const resp= await fetch(url)
-  //     const data = await resp.json()
-  //     console.log(data)
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+    console.log("Search Value:", e.target.value);
+  };
 
+  const handleCategoryChange = (category) => {
+    setSelectedCategories((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
 
-  //   }catch(error){
-  //     console.log(error)
-
-  //   }
-
-  // }
-
-
+  console.log(category);
   return (
-   <>
-    {/* <Carrousel/> */}
+    <>
+      {/* <Carrousel/> */}
 
-    {/* <h1 style={{fontSize:"24px",textAlign:"center"}}>All Movies</h1> */}
-    
-      <InputSearch/>
-      
-  
-  {/* <div> */}
-  <div className='container-check'>
-          <Category/>
-          <Category/>
-          
-        
-    </div>
+      {/* <h1 style={{fontSize:"24px",textAlign:"center"}}>All Movies</h1> */}
 
-    <div className='Card-Container'>
+      <InputSearch value={searchValue} onChange={handleInputChange} />
 
-      {data.map( (movie,ind) => (
+      {/* <div> */}
+      <div className="container-check">
+        {category.map((category, index) => (
+          <Category
+            key={index}
+            category={category}
+            checked={selectedCategories[category]}
+            onChange={() => handleCategoryChange(category)}
+          />
+        ))}
+      </div>
 
-        <Card 
-          key={ind}
-          movie={movie}
-          ind={ind}
-        ></Card>
+      <div className="Card-Container">
+        {data.map((movie, ind) => (
+          <Card key={ind} movie={movie} ind={ind}></Card>
+        ))}
+      </div>
 
-      ))}
-    </div>
-
-
-  {/* </div> */}
-      
-
- 
-
-  </>
-  )
-}
+      {/* </div> */}
+    </>
+  );
+};
